3c80487515a768d53d8163744491546f4fbf4723
Brenaki <91611155+Brenaki@users.noreply.github.com>
2021-11-04 20:33:35 -0300

    Primeiros Programas
    Primeiros Programas
    São executados no cmd

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hello World
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("My name's Victor Cerqueira");
+            Console.ReadLine();
+
+            /*
+             * Console é para chamar o cmd
+             * Write é para escrever no cmd
+             * ReadLine é para ler a linha
+             * \n é para quebrar linha
+             * WriteLine é para escrever uma linha d'baixo da outra
+             */
+
+            
+        }
+    }
+}
---
+++ Variaveis/Variaveis/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variavel String!!!
+
+            var MyText = "My name's Victor Cerqueira";
+
+                Console.WriteLine(MyText);
+                Console.ReadLine();
+                
+            /* Variavel com Tipos
+             * Int - Number Integer
+             * Float - Number Decimal ==> Double/Decimal
+             * Bool - True or False
+             * String - "Text"
+             */
+
+
+        }
+    }
+}
