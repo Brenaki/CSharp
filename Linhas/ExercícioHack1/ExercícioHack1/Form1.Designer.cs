4154ba66e049d88e4bf7738b34ff5cef312c7f2d
Brenaki <91611155+Brenaki@users.noreply.github.com>
2021-11-17 00:06:37 -0300

    Seletor de Nome
    Um programa feita para exercício, para entender mais sobre a linguagem para um Hackaton

---
+++ Linhas/ExercícioHack1/ExercícioHack1/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace ExercícioHack1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bntName = new System.Windows.Forms.Button();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.rTextName = new System.Windows.Forms.RichTextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bntName
+            // 
+            this.bntName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntName.Location = new System.Drawing.Point(33, 150);
+            this.bntName.Name = "bntName";
+            this.bntName.Size = new System.Drawing.Size(97, 37);
+            this.bntName.TabIndex = 0;
+            this.bntName.Text = "Click here";
+            this.bntName.UseVisualStyleBackColor = true;
+            this.bntName.Click += new System.EventHandler(this.bntName_Click);
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumber.Location = new System.Drawing.Point(30, 27);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(117, 21);
+            this.lblNumber.TabIndex = 1;
+            this.lblNumber.Text = "Number Name:";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(30, 102);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(46, 17);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "<null>";
+            // 
+            // rTextName
+            // 
+            this.rTextName.Location = new System.Drawing.Point(194, 24);
+            this.rTextName.Name = "rTextName";
+            this.rTextName.Size = new System.Drawing.Size(520, 290);
+            this.rTextName.TabIndex = 3;
+            this.rTextName.Text = "";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(34, 63);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(113, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.rTextName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.bntName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bntName;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.RichTextBox rTextName;
+        private System.Windows.Forms.TextBox txtName;
+    }
+}
+
---
+++ Linhas/ExercícioHack1/ExercícioHack1/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExercícioHack1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            try
+            {
+                rTextName.LoadFile("Name.txt", RichTextBoxStreamType.RichText);
+            }
+            catch
+            {
+                rTextName.SaveFile("Name.txt", RichTextBoxStreamType.RichText);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bntName_Click(object sender, EventArgs e)
+        {
+            nmrName = Convert.ToInt32(txtName.Text);
+            WhereName();
+
+        }
+
+        int nmrName;
+
+        void WhereName()
+        {
+            string dataName = rTextName.Text;
+            string[] BreakData = dataName.Split('-');
+            string nameSearch = BreakData[nmrName - 1];
+            lblName.Text = nameSearch;
+        }
+    }
+}
